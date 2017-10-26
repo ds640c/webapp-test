f1d6a1f19bc0ede41454222b96af2e4eba5cfbd9
ds640c <dnsun@yahoo.com>
2017-10-26 16:50:55 -0700

    latest web page

--- webapp-test/src/main/java/App.java
+++ webapp-test/src/main/java/App.java
@@ -1,27 +1,31 @@
 import static spark.Spark.get;
+import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.lang.math.RandomUtils;
+import java.util.Random;
+  
 
 public class App {
     public static void main(String[] args) {
     	
     	staticFiles.location("/public");
+    	port(8050);
+    	System.out.println("got to http://localhost:8050/");
     	
         get("/hello", (req, res) -> "Hello World");
         
         get("/name", (req, res) -> {
-        	
         	List<String> names = new ArrayList<String>();
         	names.add("Roger");
         	names.add("James");
         	names.add("Mary");
-        	names.add("Helga");
+        	names.add("Beatrix");
+        	 
+        	Random rand = new Random(); 
+        	int  i = rand.nextInt(4);
         	
-        	int i = RandomUtils.nextInt(3);
         	return  names.get(i);
         });
     }
